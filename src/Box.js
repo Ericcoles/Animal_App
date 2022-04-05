b5cb79343174d87a52f83edc1cf2d0e601a2174e
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-04-05 11:06:21 +0300

    Search added

--- src/Box.js
+++ src/Box.js
@@ -1,16 +1,16 @@
-import React from "react";
+// import React from "react";
 
-const Box = (props) => {
-  return (
-    <div className="box">
-      <img
-        className="image"
-        src={`https://source.unsplash.com/1600x900/?${props.name}`}
-      ></img>
-      <h2>{props.name}</h2>
-      <p>{props.likes}</p>
-    </div>
-  );
-};
+// const Box = (props) => {
+//   return (
+//     <div className="box">
+//       <img
+//         className="image"
+//         src={`https://source.unsplash.com/250x250/?${props.name}`}
+//       ></img>
+//       <h2>{props.name}</h2>
+//       <p>{props.likes}</p>
+//     </div>
+//   );
+// };
 
-export default Box;
+// export default Box;
